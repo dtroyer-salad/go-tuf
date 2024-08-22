58838318e7c341a54fc729f9d411589e5b6d0524
Dean Troyer <dean.troyer@salad.com>
2024-08-22 15:20:58 -0500

    Add build
    Signed-off-by: Dean Troyer <dean.troyer@salad.com>

--- examples/cli/tuf/cmd/build.go
+++ examples/cli/tuf/cmd/build.go
@@ -1,3 +1,20 @@
+// Copyright 2024 The Update Framework Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
 package cmd
 
 import (
@@ -17,36 +34,36 @@ const DefaultHash = "sha256"
 
 var targetPath string
 
-var targetAddCmd = &cobra.Command{
-	Use:     "target-add",
+var buildCmd = &cobra.Command{
+	Use:     "build",
 	Aliases: []string{"i"},
-	Short:   "Add a target file",
+	Short:   "Build metadata from current artifacts",
 	Args:    cobra.ExactArgs(0),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return TargetAddCmd()
+		return BuildCmd()
 	},
 }
 
 func init() {
-	targetAddCmd.Flags().StringVarP(&targetPath, "file", "f", "", "location of the file to add to a target")
-	rootCmd.AddCommand(targetAddCmd)
+	buildCmd.Flags().StringVarP(&targetPath, "file", "f", "", "location of the file to add to a target")
+	rootCmd.AddCommand(buildCmd)
 }
 
-func TargetAddCmd() error {
+func BuildCmd() error {
 	// set logger and debug verbosity level
-	metadata.SetLogger(stdr.New(stdlog.New(os.Stdout, "target_add_cmd", stdlog.LstdFlags)))
+	metadata.SetLogger(stdr.New(stdlog.New(os.Stdout, "build_cmd", stdlog.LstdFlags)))
 	if Verbosity {
 		stdr.SetVerbosity(5)
 	}
 
 	cwd, err := os.Getwd()
 	if err != nil {
-		return fmt.Errorf("target_add: getting cwd failed")
+		return fmt.Errorf("build: getting cwd failed")
 	}
 
 	cfg, trusted, err := MakeLocalUpdater(filepath.Join(cwd, DefaultMetadataDir))
 	if err != nil {
-		return fmt.Errorf("Error loading metadata: %s", err)
+		return fmt.Errorf("build: Error loading metadata: %s", err)
 	}
 
 	// fmt.Printf("cfg:\n Metadata: %s\n Targets: %s\n", cfg.LocalMetadataDir, cfg.LocalTargetsDir)
@@ -56,7 +73,7 @@ func TargetAddCmd() error {
 	localPath := filepath.Join(cwd, targetPath)
 	targetFileInfo, err := metadata.TargetFile().FromFile(localPath, "sha256")
 	if err != nil {
-		fmt.Errorf("generating target file info failed")
+		fmt.Errorf("build: generating target file info failed")
 	}
 	// fmt.Printf("trusted.Targets: %+v\ntargetFileInfo: %+v\n", Keys(trusted.Targets), targetFileInfo)
 	trusted.Targets["targets"].Signed.Targets[targetPath] = targetFileInfo
@@ -78,14 +95,14 @@ func TargetAddCmd() error {
 		key := keys[name]
 		signer, err := signature.LoadSigner(key, crypto.Hash(0))
 		if err != nil {
-			return fmt.Errorf("loading signer failed: %s", err)
+			return fmt.Errorf("build: loading signer failed: %s", err)
 		}
 		switch name {
 		case "targets":
 			trusted.Targets["targets"].ClearSignatures()
 			_, err = trusted.Targets["targets"].Sign(signer)
 			if err != nil {
-				return fmt.Errorf("signing metadata failed: %s", err)
+				return fmt.Errorf("build: signing metadata failed: %s", err)
 			}
 			filename := fmt.Sprintf("%d.%s.json", trusted.Targets["targets"].Signed.Version, name)
 			err = trusted.Targets["targets"].ToFile(filepath.Join(cfg.LocalMetadataDir, filename), true)
@@ -93,7 +110,7 @@ func TargetAddCmd() error {
 			trusted.Snapshot.ClearSignatures()
 			_, err = trusted.Snapshot.Sign(signer)
 			if err != nil {
-				return fmt.Errorf("signing metadata failed: %s", err)
+				return fmt.Errorf("build: signing metadata failed: %s", err)
 			}
 			filename := fmt.Sprintf("%d.%s.json", trusted.Snapshot.Signed.Version, name)
 			err = trusted.Snapshot.ToFile(filepath.Join(cfg.LocalMetadataDir, filename), true)
@@ -101,13 +118,13 @@ func TargetAddCmd() error {
 			trusted.Timestamp.ClearSignatures()
 			_, err = trusted.Timestamp.Sign(signer)
 			if err != nil {
-				return fmt.Errorf("signing metadata failed: %s", err)
+				return fmt.Errorf("build: signing metadata failed: %s", err)
 			}
 			filename := fmt.Sprintf("%s.json", name)
 			err = trusted.Timestamp.ToFile(filepath.Join(cfg.LocalMetadataDir, filename), true)
 		}
 		if err != nil {
-			return fmt.Errorf("saving metadata to file failed: %s", err)
+			return fmt.Errorf("build: saving metadata to file failed: %s", err)
 		}
 	}
 
